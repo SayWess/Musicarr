74ccf8a2b260cf075477c6d123ea71b5d1e9f265
SayWess <saywess.developpement@gmail.com>
2025-03-31 22:32:14 +0200

    Add images domain config. Add 404_not_found image

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["i.ytimg.com"], // Add domains for youtube images
+  },
 };
 
 export default nextConfig;
