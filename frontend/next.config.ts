30b26703e974f2d7e8d1747883505e1b17e1bee6
SayWess <saywess.developpement@gmail.com>
2025-04-02 14:59:02 +0200

    Replace deprecated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["i.ytimg.com"], // Add domains for youtube images
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+    ], // Add domains for youtube images
   },
+  // reactStrictMode: false // Uncomment this line to disable React's strict mode and prevent double rendering, but not recommended in development
 };
 
 export default nextConfig;
