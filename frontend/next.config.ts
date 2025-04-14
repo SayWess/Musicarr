5c54f344ed79327b05fe837a1dfc23586b59825c
SayWess <saywess.developpement@gmail.com>
2025-04-15 00:45:35 +0200

    Add API address to trusted domains for loading images.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,9 +7,24 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.ytimg.com",
       },
+      {
+        protocol: "http",
+        hostname: "192.168.146.221"
+      }
     ], // Add domains for youtube images
   },
   // reactStrictMode: false // Uncomment this line to disable React's strict mode and prevent double rendering, but not recommended in development
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
+  allowedDevOrigins: [
+    'local-origin.dev',
+    '*.local-origin.dev',
+  ]
+
 };
 
 export default nextConfig;
