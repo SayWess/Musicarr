a35c65020d1c427ce17f6c81b257e4c5501bb2b0
SayWess <saywess.developpement@gmail.com>
2025-04-22 23:18:25 +0200

    Fix reopen modal.

--- frontend/src/components/SortItems.tsx
+++ frontend/src/components/SortItems.tsx
@@ -77,7 +77,7 @@ export const SortVideos = ({
   return (
     <>
       {showOptions && <div className="fixed w-full h-full z-25"></div>}
-      <div className="fixed left-1/2 bottom-1 mb-10 md:mb-2 transform -translate-x-1/2 z-25 flex gap-2">
+      <div className="fixed left-1/2 bottom-1 mb-10 md:mb-2 transform -translate-x-1/2 z-20 flex gap-2">
         <button
           onClick={() => setShowOptions(!showOptions)}
           className="px-4 py-2 bg-blue-600 text-white rounded-full shadow-lg"
