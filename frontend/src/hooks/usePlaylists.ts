c1e7ed9f00854140f8498765905ce2a03b474338
SayWess <saywess.developpement@gmail.com>
2025-04-22 20:31:00 +0200

    Add playlists sort. Add animations.

--- frontend/src/hooks/usePlaylists.ts
+++ frontend/src/hooks/usePlaylists.ts
@@ -2,30 +2,38 @@ import axios from "axios";
 import useSWR, { mutate } from "swr";
 import { Playlist } from "@/types/models";
 import { endpointPlaylists } from "@/constants/endpoints";
+import { SortOrder, SortField } from "@/constants/sortFields";
 
-const usePlaylists = () => {
-  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-  const { data: playlists, error, isLoading } = useSWR(endpointPlaylists, fetcher);
 
-  // ✅ Optimistic Update: Toggle Check
+const usePlaylists = (initialPlaylist: Playlist[], sortBy: SortField, sortOrder: SortOrder) => {
+
+  const fetcher = (url: string) => {
+    return axios
+      .get(url + `?sort_by=${sortBy}&order=${sortOrder}`)
+      .then((res) => res.data);
+  };
+
+  const { data: playlists, error, isLoading } = useSWR(endpointPlaylists, fetcher, { fallbackData: initialPlaylist });
+
+  // Optimistic Update: Toggle Check
   const toggleCheckEveryDay = async (id: string) => {
     if (!playlists) return;
   
-    // 1️⃣ Récupérer la playlist et la modifier en local
+    // Récupérer la playlist et la modifier en local
     const updatedPlaylist: Playlist = playlists.find((p: Playlist) => p.id === id);
     if (!updatedPlaylist) return;
   
     const newPlaylist: Playlist = { ...updatedPlaylist, check_every_day: !updatedPlaylist.check_every_day };
   
-    // 2️⃣ Mise à jour optimiste (UI change immédiatement)
+    // Mise à jour optimiste (UI change immédiatement)
     const updatedPlaylists = playlists.map((p: Playlist) => (p.id === id ? newPlaylist : p));
     mutate(endpointPlaylists, updatedPlaylists, false);
   
     try {
-      // 3️⃣ Envoi de la playlist complète au serveur
+      // Envoi de la playlist complète au serveur
       await axios.put(`${endpointPlaylists}/${id}`, {"check_every_day": newPlaylist.check_every_day});
       
-      // 4️⃣ Revalidation après succès (pour être sûr d'avoir les bonnes données)
+      // Revalidation après succès (pour être sûr d'avoir les bonnes données)
       mutate(endpointPlaylists);
     } catch (error) {
       console.error("Error updating playlist", error);
